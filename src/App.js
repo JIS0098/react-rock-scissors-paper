0f024a531b82868a158ae79955614bc08068bb5a
Jisoo Han <llqrw55@naver.com>
2023-09-14 14:17:10 +0900

    점수내기 완성

--- src/App.js
+++ src/App.js
@@ -1,90 +1,122 @@
+
 import "./App.css";
 import "./component.css";
-import ComputerBox from "./component/ComputerBox";
-import UserBox from "./component/UserBox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
  
 
 //브금,눌렀을때 효과음 추가
 //점수기록 마무리
 //3점되면 유저승리 창 만들기 
 
-const choice = {
-  rock:{
+const item = [
+{
     name:"rock",
     img:"./images/rock.png"
   },
-  scissors:{
+{
     name:"scissors",
     img:"./images/scissors.png"
   },
-  paper:{
+{
     name:"paper",
     img:"./images/paper.png"
   }
-}
+]
 
 function App() {
-const [userSelect,setUserSelect]=useState(null);
-const [computerChoice,serComputerChoice]=useState(null);
-const [result,setResult]=useState("...");
-//const [score,setScore]=useState(null);
+let [user,setUser]=useState(
+  {
+    name:'',
+    img:'',
+    count:0,
+    result:"...",
+  }
+);
+let [computer,setComputer]=useState(
+  {
+    name:'',
+    img:'',
+    count:0
+  }
+);
+let random =Math.floor(Math.random()*item.length)
 
-
-const play=(userChoice)=>{
-  setUserSelect(choice[userChoice]);
-const computerChoice =randomChoice(); 
-  serComputerChoice(computerChoice);
-  setResult(judgment(choice[userChoice],computerChoice));
-  //setScore(userCount(result));
+function play(a){
+  let copy ={...user}
+    copy.name=item[a].name
+    copy.img=item[a].img
+    let copy2 ={...computer}
+    copy2.name=item[random].name
+    copy2.img=item[random].img
+    setComputer(copy2)
+    if(a==random){
+      copy.result='TIE'
+    }else if(a==0){
+      if(random==1){
+        copy.result='WIN!'
+        copy.count++
+      }else{
+        copy.result='LOSE'
+        copy2.count++
+      }
+    }else if(a==1){
+      if(random==2){
+        copy.result='WIN!'
+        copy.count++
+      }else{
+        copy.result='LOSE'
+        copy2.count++
+      }
+    }else if(a==2){
+      if(random==0){
+        copy.result='WIN!'
+        copy.count++
+      }else{
+        copy.result='LOSE'
+        copy2.count++
+      }
+    }
+    setUser(copy)
 }
 
-
-
-/*const userCount =(uesrResult)=>{
-  let userScore = 0;
-  if(uesrResult==="WIN!"){
-    userScore=userScore+1;
-  }else if(uesrResult==="TIE" || uesrResult==="LOSE"){
-    userScore = userScore
-  };
-  console.log(userScore)
-  return userScore
-}*/
-
-const judgment =(user,computer)=>{
-  if(user.name==computer.name){
-    return "TIE"
-  }else if(user.name==="rock")return computer.name==="scissors"? "WIN!":"LOSE"
-  else if(user.name==="scissors")return computer.name==="paper"? "WIN!":"LOSE"
-  else if(user.name==="paper")return computer.name==="rock"? "WIN!":"LOSE"
-}
-
-const randomChoice=()=>{
-  let itemArray =Object.keys(choice);
-  let randomItem = Math.floor(Math.random()*itemArray.length);
-  let final = itemArray[randomItem];
-  return choice[final];
-}
 
   return (
     <div className="main">
       <div className="game">
-        <div className="game-score">0:0</div>
+        <div className="game-score">{user.count}:{computer.count}</div>
         <div className="game-screen">
-          <UserBox item={userSelect} />
+        <div className='user-box'>
+          <h2>you</h2>
+          <Card select={user}/>
+        </div>
           <div className="game-screen-line"></div>
-          <ComputerBox item={computerChoice} />
+          <div className='computer-box'>
+          <h2>computer</h2>
+           <Card select={computer}/>
+          </div>
         </div>
         <div className="game-but">
-          <button onClick={()=>play("rock")}><img src="./images/rock icon.png"/></button>
-          <button onClick={()=>play("scissors")}><img src="./images/scissors icon.png"/></button>
-          <button onClick={()=>play("paper")}><img src="./images/paper icon.png"/></button>
+          <button onClick={()=>play(0)}><img src="./images/rock icon.png"/></button>
+          <button onClick={()=>play(1)}><img src="./images/scissors icon.png"/></button>
+          <button onClick={()=>play(2)}><img src="./images/paper icon.png"/></button>
         </div>
-        <h2 className="game-result">{result}</h2>
+        <h2 className="game-result">{user.result}</h2>
       </div>
     </div>
   );
+
+  function Card({select}){
+    return (
+      <>
+      <img className='item-img' src={select&&select.img}></img>
+      <div>{select&&select.name}</div>
+      </>
+    )
+  }
+
 };
+
+
+
 
 export default App;
